0d0fe9b19db22fce8b5d00deb65dab123732179c
Vedant Mathur <vedantmathur1079@gmail.com>
2026-04-06 17:28:04 +0530

    Update camera_stream.ino

--- code/esp32-cam/camera_stream.ino
+++ code/esp32-cam/camera_stream.ino
@@ -1,1 +1,22 @@
+#include "esp_camera.h"
+#include <WiFi.h>
 
+const char* ssid = "ESP32CAM_ROBOT";
+const char* password = "12345678";
+
+WiFiServer server(80);
+
+void startCameraServer();
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+  Serial.println("WiFi Started");
+
+  server.begin();
+  startCameraServer();
+}
+
+void loop() {
+}
