8327212acb3dd118940a12eb3f8455d093bf559e
Vedant Mathur <vedantmathur1079@gmail.com>
2026-04-06 17:27:37 +0530

    Update robot_control.ino

--- code/arduino/robot_control.ino
+++ code/arduino/robot_control.ino
@@ -1,1 +1,73 @@
+// Arduino Uno + L298N Robot Control
 
+const int IN1 = 8;
+const int IN2 = 9;
+const int IN3 = 10;
+const int IN4 = 11;
+const int ENA = 5;
+const int ENB = 6;
+
+int speedVal = 180;
+
+void setup() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  Serial.begin(115200);
+
+  analogWrite(ENA, speedVal);
+  analogWrite(ENB, speedVal);
+}
+
+void forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void backward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void left() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void right() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void stopMotor() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void loop() {
+  if (Serial.available()) {
+    char cmd = Serial.read();
+
+    switch (cmd) {
+      case 'f': forward(); break;
+      case 'b': backward(); break;
+      case 'l': left(); break;
+      case 'r': right(); break;
+      case 's': stopMotor(); break;
+    }
+  }
+}
